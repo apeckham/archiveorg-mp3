ff937a54aaaabd5a73e952b5fee4a25b20c74e27
apeckham <apeckham@localhost>
2023-06-22 11:01:26 -0700

    only print ocmmands

--- download.rb
+++ download.rb
@@ -3,11 +3,10 @@ require 'nokogiri'
 
 xml = Nokogiri::XML(open(ARGV[0]))
 xml.xpath('//file').each do |file|
-  if file['source'] == 'original'
+  if file['source'] == 'original' && file['name'] =~ /\.mp4/
     filename = File.basename(file['name'], '.*')
     file_url = File.join(File.dirname(ARGV[0]), file['name'])
     output_filename = "#{filename}.mp3"
-    command = %Q(curl -s "#{file_url}" | ffmpeg -i pipe:0 -b:a 192K -vn "#{output_filename}" && ffmpeg-normalize "#{output_filename}" -o "#{output_filename}" -f)
-    system(command) || raise("An error occurred while executing the command: #{command}")
+    puts %Q(curl -s "#{file_url}" | ffmpeg -i pipe:0 -b:a 192K -vn "#{output_filename}" && ffmpeg-normalize "#{output_filename}" -o "#{output_filename}" -f)
   end
 end
