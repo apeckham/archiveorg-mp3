304ae0a06348436c3d6cbf06cf89e17b0bf288d6
Aaron <apeckham@gmail.com>
2023-06-23 00:34:01 +0000

    one line

--- download.rb
+++
@@ -1,8 +0,0 @@
-require 'open-uri'
-require 'nokogiri'
-
-Nokogiri::XML(open(ARGV[0])).xpath('//file').each do |node|
-  if node['source'] == 'original' && node['name'] =~ /\.mp4/
-    file.puts File.join(File.dirname(ARGV[0]), node['name'])
-  end
-end
--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
+#!/usr/bin/env sh
+
+set -ex
+
+ruby -r open-uri -r nokogiri -e "puts Nokogiri::XML(URI.open(ARGV[0])).xpath('//file').select {|node| node['source'] == 'original' && node['name'] =~ /\.mp4/}.map {|node| File.join(File.dirname(ARGV[0]), node['name'])}" "$@" > urls.txt
+
 aria2c -i urls.txt --on-download-complete=postprocess.sh
