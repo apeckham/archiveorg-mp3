2df7a81f9cfde8a7ed5f2993ed82c5debd3fb6b5
Aaron <apeckham@gmail.com>
2023-06-23 00:43:13 +0000

    fix postprocess path

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ set -ex
 
 ruby -r open-uri -r nokogiri -e "puts Nokogiri::XML(URI.open(ARGV[0])).xpath('//file').select {|node| node['source'] == 'original' && node['name'] =~ /\.mp4/}.map {|node| File.join(File.dirname(ARGV[0]), node['name'])}" "$@" > urls.txt
 
-aria2c -i urls.txt --on-download-complete=postprocess.sh
+aria2c -i urls.txt --on-download-complete=./postprocess.sh
