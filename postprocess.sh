635a78c8a3540f535fcdbc78addbb9982782d553
Aaron <apeckham@gmail.com>
2023-06-23 00:27:50 +0000

    fix normalize args

--- postprocess.sh
+++ postprocess.sh
@@ -8,9 +8,11 @@ base="${filename%.*}"
 # Get the audio bitrate from the source file
 bitrate=$(ffprobe -v error -select_streams a:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1 "$1")
 
-# Convert to mp3 using the same bitrate, then normalize
+# Convert to mp3 using the same bitrate
 ffmpeg -i "$1" -b:a ${bitrate} -vn "${base}.mp3"
-ffmpeg-normalize "${base}.mp3" -o "${base}.mp3" -f
+
+# Normalize the audio levels
+ffmpeg-normalize "${base}.mp3" -o "${base}.mp3" -f -c:a libmp3lame
 
 # Remove the original file
 rm "$1"
